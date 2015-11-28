a0bfd19ad3233ae5296eca179a0bcd2eb9e83811
Oliver Powell <oliverpowell84@gmail.com>
2015-11-28 17:21:13 +0200

    More querying Clojure collections with datomic.

--- src/clojure_learning/datomic/querying_clojure_collections.clj
+++ src/clojure_learning/datomic/querying_clojure_collections.clj
@@ -1,9 +1,9 @@
 (ns clojure-learning.datomic.querying-clojure-collections
   (:require [datomic.api :as d :refer [db q]]))
 
-;; examples based on  https://gist.github.com/stuarthalloway/2645453
+;; Examples based on  https://gist.github.com/stuarthalloway/2645453.
 
-;; just a scalar binds ?answer to the scalar
+;; Just a scalar binds ?answer to the scalar.
 ;; result: #{[42]}
 (q '[:find ?answer
      :in ?answer]
@@ -14,13 +14,13 @@
      :in ?answer]
    :hello)
 
-;; binding multiple scalars
+;; Binding multiple scalars.
 ;; result: #{["Bruce" 30]}
 (q '[:find ?name ?age
      :in ?name ?age]
    "Bruce" 30)
 
-;; how about a relation?
+;; How about a relation?
 ;; result: #{["Bruce"] ["Alice"] ["Peter"]}
 (q '[:find ?first
      :in [[?first ?last]]]
@@ -28,7 +28,7 @@
     ["Peter" "Parker"]
     ["Alice" "Parker"]])
 
-;; adding a where clause requires an actual database source, these begin
+;; Adding a where clause requires an actual database source, these begin
 ;; with '$'. here we are essentially mocking out a database source as a list
 ;; of tuples. looks like we aren't forced to use 4 tuples [E A V T].
 ;; result: #{["Wayne"]}
@@ -40,7 +40,7 @@
     ["Peter" "Parker"]
     ["Alice" "Parker"]])
 
-;; it would be annoying to have to include the database source each time for a single
+;; It would be annoying to have to include the database source each time for a single
 ;; database source, we can omit $a, and the query will use it's first argument as the
 ;; source.
 ;; same result as above.
@@ -51,7 +51,7 @@
     ["Peter" "Parker"]
     ["Alice" "Parker"]])
 
-;; how about finding all first names when the last name is 'Parker' ?
+;; How about finding all first names when the last name is 'Parker' ?
 ;; result: #{["Alice"] ["Peter"]}.
 (q '[:find ?first
      :where
@@ -60,7 +60,7 @@
     ["Peter" "Parker"]
     ["Alice" "Parker"]])
 
-;; a simple in memory join of two tuples.
+;; A simple in memory join of two tuples.
 ;; result: #{["John" 71]}
 (q '[:find ?first ?id
      :in [?last ?first ?email] [?email ?id]]
@@ -75,3 +75,101 @@
     ["Wayne" "Bruce" "bwayne@thebatcave.com"]]
    [["bwayne@thebatcave.com" 71]
     ["pparker@thelab.com" 55]])
+
+;; The same result as above, but using database expressions instead of relational bindings (apparently faster).
+;; Quite interesting that you can query over tuples of things, as if they were a database. Gives us
+;; some clues as to datomic's internals.
+(q '[:find ?first ?id
+     :in $a $b
+     :where
+     [$a ?last ?first ?email]
+     [$b ?email ?id]]
+   [["Parker" "Peter" "pparker@thelab.com"]
+    ["Wayne" "Bruce" "bwayne@thebatcave.com"]]
+   [["bwayne@thebatcave.com" 71]
+    ["pparker@thelab.com" 55]])
+
+;; Let's take this a little further, and test our not clauses, and not joins, to explore how they work, and where
+;; they'd be useful.
+
+;; If one of our tuples has less elements than it should, then you'll get an error.
+;; Notice Oliver Queen's entry uses his full name as a single sting, interestingly, this
+;; giwes an IndexOutOfBounds exception. Moral of the story make sure your tuple size matches
+;; your bound variables.
+(q '[:find ?first ?id
+     :in $a $b
+     :where
+     [$a ?last ?first ?email]
+     [$b ?email ?id]]
+   [["Parker" "Peter" "pparker@thelab.com"]
+    ["Wayne" "Bruce" "bwayne@thebatcave.com"]
+    ["Tony" "Stark" "tony@starkindustries.com"]
+    ["Oliver Queen" "oliver@queenenterprises.com"]]
+   [["bwayne@thebatcave.com" 71]
+    ["pparker@thelab.com" 55]])
+
+;; More variables than required doesn't cause any errors.
+(q '[:find ?first ?id
+     :in $a $b
+     :where
+     [$a ?last ?first ?email]
+     [$b ?email ?id]]
+   [["Parker" "Peter" "pparker@thelab.com"]
+    ["Wayne" "Bruce" "bwayne@thebatcave.com"]
+    ["Tony" "Stark" "tony@starkindustries.com"]
+    ["Oliver" "Queen" "oliver@queenenterprises.com" "Extra"]]
+   [["bwayne@thebatcave.com" 71]
+    ["pparker@thelab.com" 55]])
+
+;; Find all people, that don't have an id number.
+(q '[:find ?last ?first
+      :in $a $b
+      :where
+      [$a ?last ?first ?email]
+     (not [$b ?email _])]
+    [["Parker" "Peter" "pparker@thelab.com"]
+     ["Wayne" "Bruce" "bwayne@thebatcave.com"]
+     ["Tony" "Stark" "tony@starkindustries.com"]
+     ["Oliver" "Queen" "oliver@queenenterprises.com"]]
+    [["bwayne@thebatcave.com" 71]
+     ["pparker@thelab.com" 55]])
+
+;; Find all people, who are not Spiderman. Notice that we are forced to use
+;; not-join here. At first I thought you could just use two not clauses separately,
+;; but then you have the issue that ?id doesn't get bound.
+(q '[:find ?last ?first
+     :in $a $b $c
+     :where
+     [$a ?last ?first ?email]
+     (not-join [?email]
+               [$b ?email ?id]
+               [$c ?id "Spiderman"])]
+   [["Parker" "Peter" "pparker@thelab.com"]
+    ["Wayne" "Bruce" "bwayne@thebatcave.com"]
+    ["Tony" "Stark" "tony@starkindustries.com"]
+    ["Oliver" "Queen" "oliver@queenenterprises.com"]]
+   [["bwayne@thebatcave.com" 71]
+    ["pparker@thelab.com" 55]]
+   [[71 "Batman"]
+    [55 "Spiderman"]])
+
+;; Attempting to use two not clauses instead fails with:
+;; [?id] not bound in not clause: (not-join [?email ?id] [$b ?email ?id])
+;; Looks like (not ?a ?b) is coverted to a (not-join [?a ?b] ...) under the hood.
+;; Also it makes sense that ?id might not be bound, making the final clause
+;; impossible to check.
+(q '[:find ?last ?first
+     :in $a $b $c
+     :where
+     [$a ?last ?first ?email]
+     (not [$b ?email ?id])
+     (not [$c ?id "Spiderman"])]
+   [["Parker" "Peter" "pparker@thelab.com"]
+    ["Wayne" "Bruce" "bwayne@thebatcave.com"]
+    ["Tony" "Stark" "tony@starkindustries.com"]
+    ["Oliver" "Queen" "oliver@queenenterprises.com"]]
+   [["bwayne@thebatcave.com" 71]
+    ["pparker@thelab.com" 55]]
+   [[71 "Batman"]
+    [55 "Spiderman"]])
+
